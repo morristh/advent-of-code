af22c1cafdc832123268ee1f73b15fc17460bbd3
unknown <morris.thanell@gmail.com>
2024-05-13 21:08:21 +0200

    Finished day 3

--- 2023/day3/3.py
+++ 2023/day3/3.py
@@ -1,10 +1,6 @@
 import re
 
-
-
-
-
-with open('./test.txt', 'r') as file:
+with open('./input3.txt', 'r') as file:
     input = file.readlines()
 
 
@@ -33,61 +29,32 @@ for i, line in enumerate(input):
         if len(nbrSymbolsAroundString(i, nbr.start(), nbr.end(), r'[^\d|.|\s]')) > 0:
             sum += int(nbr.group())
 
-    # Task 2
+print(f'Sum of valid numbers: {sum}')
+
+# Task 2
+
+with open('input3.txt', 'r') as file:
+    input = file.readlines()
+
+gear_sum = 0
+for i, line in enumerate(input):
     gears = re.finditer(r'\*', line)
     for gear in gears:
-        nbrs = nbrSymbolsAroundString(i, 0, len(line), r'\d+')
-        gearNbrs = []
-        for nbr in nbrs:
-            # print(f'number: {nbr}')
-            for i in range(gear.start()-1, gear.end()+1):
-                # print(f'i: {i}')
-                # print(f'span: {nbr.span()}')
-                if i in range(*nbr.span()):
-                    gearNbrs.append(int(nbr.group()))
-                    # print('break!')
-                    break
-        if len(gearNbrs) >= 2:
-            gearRatioSum += int(gearNbrs[0]) * int(gearNbrs[1])
+        if i == 0:
+            inputRange = input[:2]
+        elif i == len(input)-1:
+            inputRange = input[-2:]
+        else:
+            inputRange = input[i-1:i+2]
+        valid_nbrs = []
+        for nbrs_list in inputRange:
+            nbrs = re.finditer(r'\d+', nbrs_list)
+            for nbr in nbrs:
+                if gear.start() in range(nbr.start() - 1, nbr.end()+1):
+                    valid_nbrs.append(int(nbr.group()))
+        if len(valid_nbrs) == 2:
+            gear_sum += valid_nbrs[0] * valid_nbrs[1]
 
-print(f'Sum of valid numbers: {sum}')
-print(f'Sum of gear ratios: {gearRatioSum}')
+print(f'Gear ratio sum: {gear_sum}')
 
-
-
-
-
-# # Task 2
-
-# def isValidGear(lineNbr, start):
-#     if lineNbr == 0:
-#         inputRange = input[:2]
-#     elif lineNbr == len(input)-1:
-#         inputRange = input[-2:]
-#     else:
-#         inputRange = input[lineNbr-1:lineNbr+2]
-
-#     adjacentNbrs = 0
-#     gearRatio = 1
-#     for line in inputRange:
-#         line = line[:-2] # Kanske onödig rad...
-#         if start == 0: start = 1
-#         m = re.search(r'\d', line[start-1:start+2])
-#         if m: 
-#             adjacentNbrs +=1
-#             gearRatio *= int(m.group())
-#     if adjacentNbrs == 2:
-#         return gearRatio
-#     else:
-#         return 0
-
-
-# gearSum = 0
-# for i, line in enumerate(input):
-#     gears = re.finditer(r'\*', line)
-#     for gear in gears:
-#         gearRatioSum = isValidGear(i, gear.start())
-#         if gearRatioSum != 0:
-#             gearSum += gearRatioSum
-
-# print(f'Total gear ratio sum: {gearSum}') # DEL 2 funkar ej än
+        
